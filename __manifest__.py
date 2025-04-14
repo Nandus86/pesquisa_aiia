f945643bcf6b4d09032f33884905704928e759eb
Nandus86 <nandusempreendimentos@gmail.com>
2025-04-14 16:12:12 -0300

    v2.0.8

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
         armazená-las como leads e fornecer ações rápidas (WhatsApp, Email, etc.).
     """,
     'category': 'Sales/CRM',
-    'author': 'Fernando Dias - v2.0.7',
+    'author': 'Fernando Dias - v2.0.8',
     'website': '',
     'license': 'LGPL-3', 
     'depends': [
