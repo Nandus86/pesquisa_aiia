79b1843b04e14ff26faa2dea9cc4a0d99bae2d90
Nandus86 <nandusempreendimentos@gmail.com>
2025-04-14 13:54:27 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
         armazená-las como leads e fornecer ações rápidas (WhatsApp, Email, etc.).
     """,
     'category': 'Sales/CRM',
-    'author': 'Fernando Dias - v2.0.2',
+    'author': 'Fernando Dias - v2.0.3',
     'website': '',
     'license': 'LGPL-3', 
     'depends': [
@@ -18,9 +18,9 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+        'views/pesquisa_aiia_scrape_wizard_views.xml',
         'views/pesquisa_aiia_lead_views.xml',
-        'views/res_config_settings_views.xml',
-        'views/pesquisa_aiia_scrape_wizard_views.xml'
+        'views/res_config_settings_views.xml'
     ],
     'installable': True,
     'application': True, 
