dc3d0c2265a3fc831b33d821d6aaf4cdf5e9fbf2
Nandus86 <nandusempreendimentos@gmail.com>
2025-04-14 19:22:54 -0300

    v2.0.10

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
         armazená-las como leads e fornecer ações rápidas (WhatsApp, Email, etc.).
     """,
     'category': 'Sales/CRM',
-    'author': 'Fernando Dias - v2.0.9',
+    'author': 'Fernando Dias - v2.0.10',
     'website': '',
     'license': 'LGPL-3', 
     'depends': [
@@ -18,6 +18,7 @@
     ],
     'data': [
         'security/ir.model.access.csv', 
+        'views/pesquisa_aiia_server_actions.xml',
         'views/pesquisa_aiia_lead_views.xml',          
         'views/pesquisa_aiia_search_views.xml',        
         'views/res_config_settings_views.xml'
